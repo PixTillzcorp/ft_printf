5587611770848dc828a64e375db32438a3fd1871
PixTillzcorp <heinfalt@student.42.fr>
2017-03-16 14:41:51 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -12,6 +12,18 @@
 
 #include "ft_printf.h"
 
+void	choice(char *format)
+{
+	if (ft_isalpha(*format))
+		flag_conv(*format);
+	else if (*format == ('#' || ' ' || '+' || '-' || '0'))
+		flag_cara(*format);
+	else if (*format == '.')
+		flag_presc(*format)
+	else if (*format == '%')
+		ft_putchar('%');
+}
+
 int		ft_printf(const char *format, ...)
 {
 	va_list args;
@@ -21,20 +33,35 @@ int		ft_printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			if (*(format + 1) == 'd' || *(format + 1) == 'D' || *(format + 1) == 'i')
+			// if (*(format + 1) == '.')
+			// {
+			// 	if (*(format + 2) == '*')
+
+			// }
+			if (*(format + 1) == 'd' || *(format + 1) == 'i')
 				ft_putnbr(va_arg(args, int));
+			if (*(format + 1) == 'D')
+				ft_putlnbr(va_arg(args, long long int));
 			if (*(format + 1) == 's')
 				ft_putstr(va_arg(args, char *));
-			if (*(format + 1) == '%')
-				ft_putchar('%');
+			if (*(format + 1) == 'S')
+				ft_putlstr(va_arg(args, wchar_t *));
+			if (*(format + 1) == 'c')
+				ft_putchar(va_arg(args, int));
+			if (*(format + 1) == 'C')
+				ft_putlchar(va_arg(args, wchar_t));
 			if (*(format + 1) == 'e' || *(format + 1) == 'E')
 				ft_putstr(ft_dec_to_sci(va_arg(args, double), *(format + 1), "6"));
 			if (*(format + 1) == 'o')
 				ft_putstr(ft_dec_to_base(va_arg(args, unsigned int), 8));
+			if (*(format + 1) == 'O')
+				ft_putstr(ft_ldec_to_base(va_arg(args, long long int), 8));
 			if (*(format + 1) == 'x' || *(format + 1) == 'X')
 				ft_putstr(ft_dec_to_hex(va_arg(args, unsigned int), *(format + 1)));
 			if (*(format + 1) == 'p')
 				ft_putstr(ft_ptr_to_hex(va_arg(args, void *)));
+			if (*(format + 1) == '%')
+				ft_putchar('%');
 			format += 2;
 		}
 		else
